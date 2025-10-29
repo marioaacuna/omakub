23409140bfda3503d6b1915b9b6143c044672ad2
Mario <marioacuna@gmx.ch>
2025-10-29 16:04:38 +0100

    fix(first-run-choices.sh): update selected programming languages to only include Rust

--- install/first-run-choices.sh
+++ install/first-run-choices.sh
@@ -8,7 +8,7 @@ if [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]]; then
 fi
 
 AVAILABLE_LANGUAGES=("Ruby on Rails" "Node.js" "Go" "PHP" "Python" "Elixir" "Rust" "Java")
-SELECTED_LANGUAGES="Ruby on Rails","Node.js"
+SELECTED_LANGUAGES="Rust"
 export OMAKUB_FIRST_RUN_LANGUAGES=$(gum choose "${AVAILABLE_LANGUAGES[@]}" --no-limit --selected "$SELECTED_LANGUAGES" --height 10 --header "Select programming languages")
 
 AVAILABLE_DBS=("MySQL" "Redis" "PostgreSQL")
